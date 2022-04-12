8a9f1c42d74eaf115aac6226e289a69c9b5e4c42
Lazaro-oliveira <lazarobpmg@hotmail.com>
2022-04-11 22:08:55 -0300

    Criação classe Cliente.java

--- src/br/wipro/vanillajava/ContaCorrente.java
+++ src/br/wipro/vanillajava/ContaCorrente.java
@@ -1,6 +1,14 @@
 package br.wipro.vanillajava;
 
 public class ContaCorrente extends Conta {
+
+    public ContaCorrente(int numero, double saldo, String cartaoDeCredito, Cliente cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cartaoDeCredito = cartaoDeCredito;
+        this.cliente = cliente;
+    }
+
     @Override
     public void sacar(double valor) {
         if (valor > this.getSaldo()) throw new IllegalArgumentException("Saldo insuficiente");
