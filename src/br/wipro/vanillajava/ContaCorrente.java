91cc2876bbdc2ed85d6b62c55459786e8bad9d25
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-11 21:45:45 -0300

    Finalizada a classe ContaCorrente.java

--- src/br/wipro/vanillajava/ContaCorrente.java
+++ src/br/wipro/vanillajava/ContaCorrente.java
@@ -2,6 +2,10 @@ package br.wipro.vanillajava;
 
 public class ContaCorrente extends Conta {
 
+    public ContaCorrente(int numero, double saldo, String cartaoDeCredito, Cliente cliente) {
+        super(numero, saldo, cartaoDeCredito, cliente);
+    }
+
     @Override
     public void sacar(double valor) {
         if (valor > this.getSaldo()) throw new IllegalArgumentException("Saldo insuficiente");
